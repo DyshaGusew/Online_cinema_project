bd39c9937e9a522887b557753d17267efbe68fa6
DyshaGusew <pav060210@gmail.com>
2022-12-13 18:45:01 +1000

    Имя Андрей

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello, my name Andrew!");   //Если ты это видишь, то ты красавчик
+    //Выведи сюда свое имя printf("Hello, my name ______!");  и отправь на гитхаб
+
+    return 0;
+}
