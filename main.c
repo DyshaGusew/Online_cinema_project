2aa9b84f1f4cbb5487147616028be268fe290e6f
DyshaGusew <pav060210@gmail.com>
2022-12-16 17:33:41 +1000

    ggg

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 
 int main(){
     printf("Hello, my name Andrew!");   //Если ты это видишь, то ты красавчик
-    //sdfsdfsdf
     //Выведи сюда свое имя printf("Hello, my name ______!");  и отправь на гитхаб
 
     return 0;
