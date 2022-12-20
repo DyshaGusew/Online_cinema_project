89407f52b73c197067d5fb631248ff15ce8c8bb4
DyshaGusew <pav060210@gmail.com>
2022-12-20 19:27:20 +1000

    Полная реализация 1 пункта с пустыми функциями избранного и меню.

--- functions.c
+++ functions.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <locale.h>
 
+
+//Технические функции для создания списка
 //Создание структуры фильма из входных значений
 struct film* create_film(char name_film[50], int year_issue, char country[50], char genres[50], float rating){
     //Выделение памяти для фильма
@@ -12,7 +14,7 @@ struct film* create_film(char name_film[50], int year_issue, char country[50], c
     //Занесение значений в структуру фильма
     strcpy(film->name, name_film);
     film -> year_issue = year_issue;
-    strcpy(film->country, country);
+    strcpy(film->country, country);  //Так надо для строк
     strcpy(film->genres, genres);
     film->rating = rating;
     film -> next = NULL;
@@ -21,11 +23,97 @@ struct film* create_film(char name_film[50], int year_issue, char country[50], c
     return film;
 }
 
+//Создание списка фильмов
+void create_list_films(FILE *films_storage, struct film *list_root) {
+    struct film* film_read;      //Считываемый фильм
+
+    //Переменные для считывания
+    char null[3];
+    char name_film[50];
+    int year_issue;
+    char country[50];
+    char genres[50];
+    float rating;
+
+    //Считываю значения с файла и ввожу в структуру фильма.
+    //Одновременно и считываю и проверяю, а есть ли что считывать
+    while(fgets(name_film, 50, films_storage) != NULL){
+        int i = 0;
+        while(name_film[i] != '\n'){                 //Символ перехода на новую строку заменяется на 0
+            i++;
+        }
+        name_film[i]=0;
+
+        //Это для считывания года выпуска
+        fscanf(films_storage, "%d", &year_issue);
+        fgets(null, 3, films_storage);           //Переход на новую строку, тк fscanf этого не делает
+
+
+        fgets(country, 50, films_storage);
+        i =0;
+        while(country[i] != '\n'){                       //Символ переход на новую строку заменяется на 0
+            i++;
+        }
+        country[i]=0;
+
+        fgets(genres, 50, films_storage);
+        i =0;
+        while(genres[i] != '\n'){                     //Символ переход на новую строку заменяется на 0
+            i++;
+        }
+        genres[i]=0;
+
+        fscanf(films_storage, "%f", &rating);   //Переходим на новую строку
+
+        fgets(null, 3, films_storage);
+
+        film_read = create_film(name_film, year_issue, country, genres, rating);   //Занесение значения в структуру
+        adding_end(list_root, film_read);                              //Добавление к root новых элементов
+    }
+    deletion_start(list_root);                                                     //Удаление нулевого(начального) элемента
+
+    list_root->previous = film_read;                                               //Предыдущий у первого равен последнему
+    film_read->next=list_root;                                                     //Следующий у последнего равен первому
+}
+
+//Добавление элемента в конец
+void adding_end(struct  film* list_root, struct  film* added_element){
+    struct film* current = list_root;
+    while(current->next != NULL){
+        current = current -> next;
+    }
+    current->next = added_element;      //Добавление указания к последующему элементу
+    added_element->previous = current;  //Добавление указания на предыдущий
+}
+
+//Удаление первого элемента
+void deletion_start(struct film* list_root){
+    //Заношу все из второго в первый
+    list_root->year_issue = list_root->next->year_issue;
+    list_root->rating = list_root->next->rating;
+    for(int i = 0; i <50; i++){
+        list_root->name[i] = list_root->next->name[i];
+        list_root->genres[i]= list_root->next->genres[i];
+        list_root->country[i] = list_root->next->country[i];
+    }
+
+
+    //Следующий и предыдущий у первого меняю на следующий и предыдущий у второго
+    list_root->next = list_root->next->next;
+    //Предыдущий у следующего равен нынешнему
+    list_root->next->previous = list_root;
+}
+
+//-------------------------------------------------------------------
+
+//Функции вывода карточек и избранного
 //Вывод триплета карточек фильмов
 void out_list_films(struct film* list_root){
     setlocale(LC_ALL, "Russian");
 
     //О боже, сложно понять, но попробуйте
+    printf("                                                         Меню");
+    printf("                                                                                                                      %c\n", (char)24);
     printf("                                        ");                              printf("//===================================\\");printf("\\ \n");
     printf("                                        ");                              printf("||                                   ||\n");
     printf("/-----------------------------------\\   ");                              printf("||                                   ||   ");                        printf("/-----------------------------------");printf("\\ ");
@@ -45,7 +133,32 @@ void out_list_films(struct film* list_root){
     printf("\\-----------------------------------/   ");                               printf("||                                   ||   ");                        printf("\\-----------------------------------/\n");
     printf("                                        ");                               printf("||                                   ||\n");
     printf("                                        ");     printf("\\");      printf("\\===================================//\n");
+    printf("                                            Добавить фильм в избранное - 'e'\n");
+    printf("                                                           %c\n", (char)25);
+    printf("                                                  Подробная информация\n");
+
+
 }
+
+//Вывод подробной информации о фильме
+void out_detailed_films(struct film* list_root){
+    printf("                              /---------------------------------------------------------\\\n");
+    printf("                              |                                                         |\n");
+    printf("                              |                                                         |\n");
+    printf("                              |                                                         |\n");
+    printf("                              ");out_str_center(list_root->name, 57); printf("\n");
+    printf("                              |                                                         |\n");
+    printf("                              |                       Рейтинг %.1f                       |\n", list_root->rating);
+    printf("                              ");out_str_genres_center(list_root->genres, 57);printf("\n");
+    printf("                              ");out_str_country_center(list_root->country, 57);printf("\n");
+    printf("                              |                    Год выпуска: %d                    |\n", list_root->year_issue);
+    printf("                              |                                                         |\n");
+    printf("                              |                                                         |\n");
+    printf("                              |                                                         |\n");
+    printf("                              \\---------------------------------------------------------/\n");
+
+}
+
 
 //Вывод строки в середине указанного диапазона
 void out_str_center(char string[], int leight_str){
@@ -81,86 +194,88 @@ void out_str_center(char string[], int leight_str){
 
 };
 
-//Создание списка фильмов
-void create_list_films(FILE *films_storage, struct film *list_root) {
-    struct film* film_read;      //??????? ?????(?????????? ?? ??????? ?????? ?? ??????)
-
-    //Переменные для считывания
-    char null[3];                //?????????? ??? ???????? ?? ????? ??????
-    char name_film[50];
-    int year_issue;
-    char country[50];
-    char genres[50];
-    float rating;
-
-    //Считываю значения с файла и ввожу в структуру фильма
-    while(fgets(name_film, 50, films_storage) != NULL){
-        int i = 0;
-        while(name_film[i] != '\n'){                 //Символ переход на новую строку заменяется на 0
-            i++;
-        }
-        name_film[i]=0;
-
-        //Это для перехода новую строку
-        fscanf(films_storage, "%d", &year_issue);
-
-        fgets(null, 3, films_storage);           //Переход на новую строку, тк после fscanf этого не делает
-
-
-        fgets(country, 50, films_storage);
-        i =0;
-        while(country[i] != '\n'){                       //Символ переход на новую строку заменяется на 0
-            i++;
-        }
-        country[i]=0;
-
-        fgets(genres, 50, films_storage);
-        i =0;
-        while(genres[i] != '\n'){                     //Символ переход на новую строку заменяется на 0
-            i++;
-        }
-        genres[i]=0;
-
-        fscanf(films_storage, "%f", &rating);   //Переходим на новую строку
-
-        fgets(null, 3, films_storage);
-
-        film_read = create_film(name_film, year_issue, country, genres, rating);   //Занесение значения в структуру
-        adding_end(list_root, film_read);                              //Добавление к root новых элементов
+//Вывод строки жанра в середине указанного диапазона
+void out_str_genres_center(char string[], int leight_str){
+    int i = 0;
+    while(string[i] != 0){                 //Подсчет символов
+        i++;
     }
-    deletion_start(list_root);                                                     //Удаление нулевого элемента
 
-    list_root->previous = film_read;                                               //Предыдущий у первого равен последнему
-    film_read->next=list_root;                                                     //Следующий у последнего равен первому
-}
+    //-6 тк надпись "жанр: " занимает 6 символов
+    int space = (leight_str-i-6)/2;
+    int space_left = space;
+    int space_right = space;
 
-//Добавление элемента в конец
-void adding_end(struct  film* list_root, struct  film* added_element){
-    struct film* current = list_root;
-    while(current->next != NULL){
-        current = current -> next;
+    printf("|");  //Левая часть
+    while(space_left!=0){
+        printf(" ");
+        space_left--;
     }
-    current->next = added_element;      //Добавление указания к последующему элементу
-    added_element->previous = current;  //Добавление указания на предыдущий
-}
 
-//Удаление первого элемента
-void deletion_start(struct film* list_root){
-    //Заношу все из второго в первый
-    list_root->year_issue = list_root->next->year_issue;
-    list_root->rating = list_root->next->rating;
-    for(int i = 0; i <50; i++){
-        list_root->name[i] = list_root->next->name[i];
-        list_root->genres[i]= list_root->next->genres[i];
-        list_root->country[i] = list_root->next->country[i];
+    printf("Жанр: %s", string);   //Центр
+
+    while(space_right!=0){    //Правая часть
+        printf(" ");
+        space_right--;
     }
 
 
-    //Следующий и предыдущий у первого меняю на следующий и предыдущий у второго
-    list_root->next = list_root->next->next;
-    //Предыдущий у следующего равен нынешнему
-    list_root->next->previous = list_root;
+    if((leight_str-i-6)%2 != 0 ){          //Проверка на нечетность строки и сдвиг по необходимости
+        printf(" |");
+    }
+    else{
+        printf("|");
+    }
+
+};
+
+//Вывод строки страны в середине указанного диапазона
+void out_str_country_center(char string[], int leight_str){
+    int i = 0;
+    while(string[i] != 0){                 //Подсчет символов
+        i++;
+    }
+
+    //-19 тк надпись "Страна(ы) выпуска: " занимает 19 символов
+    int space = (leight_str-i-19)/2;
+    int space_left = space;
+    int space_right = space;
+
+    printf("|");  //Левая часть
+    while(space_left!=0){
+        printf(" ");
+        space_left--;
+    }
+
+    printf("Страна(ы) выпуска: %s", string);   //Центр
+
+    while(space_right!=0){    //Правая часть
+        printf(" ");
+        space_right--;
+    }
+
+
+    if((leight_str-i-19)%2 != 0 ){          //Проверка на нечетность строки и сдвиг по необходимости
+        printf(" |");
+    }
+    else{
+        printf("|");
+    }
+
+};
+
+//---------------------------------------------------------------
+
+//Функции избранного
+void return_film_favorites(struct film* film_favorites){
+    printf("Избранный фильм - %s\n", film_favorites->name);
 }
 
+//--------------------------------------------------------------
+//Функции Меню
+void out_menu(){
+    //Возвращаться к списку фильмов мы уже должны через меню можно опять использовать кнопки s и w, чтобы в нем взаимодействовать(просто через s вернуться нельзя, надо выбрать необходимый пункт меню)
+    printf("Здесь должно находиться меню");
+}
 
 
--- main.c
+++ main.c
@@ -2,36 +2,93 @@
 #include <locale.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "move_functions.h"
 
 
 int main() {
-
     //Локализация
     setlocale(LC_ALL, "Russian");
-    //Считывание всех фильмов кинотеатра
-    FILE *films_storage = fopen("films.txt", "r");
 
     //Создание каталога из всех фильмов
-    struct film* root_film_catalog;
+    FILE *films_storage = fopen("films.txt", "r");
+    struct film* root_film_catalog;     //Начальный элемент каталога
     root_film_catalog = create_film("Null", 0, "NULL", "NULL", 0.0f);
     create_list_films(films_storage, root_film_catalog);
 
-    //Вывожу фильм первый раз
+    //Вывожу три карточки первый раз
+    system( "cls" );                 //Очистка консоли
     out_list_films(root_film_catalog);
 
-    //Потом необходимо добавить условие выхода
+
+    //Главный цикл взаимодействия с карточками и меню
+    int flag_line_card = 1;       //Флаги, отслеживающие в каком мы сейчас блоке
+    int flag_menu = 0;
     while(1){
+        //Считывание введенного символа
         char sim = getch();
-        if(sim == 'd'){
+
+        //Перемещение вправо(все взаимодействия могут проходить только если мы не в меню)
+        if(sim == 'd' & flag_menu == 0){          //Очищаем консоль и выводим все, когда в середине правый элемент, перемещаем root
             system( "cls" );
+            flag_line_card = 1;
             out_list_films(root_film_catalog->next);
             root_film_catalog = root_film_catalog->next;
+
         }
-        if(sim == 'a'){
+
+        //Перемещение влево
+        if(sim == 'a' & flag_menu == 0){    //Очищаем консоль и выводим все, когда в середине левый элемент, перемещаем root
             system( "cls" );
+            flag_line_card = 1;
             out_list_films(root_film_catalog->previous);
             root_film_catalog = root_film_catalog->previous;
+        }
+
+        //Вывод подробной информации
+        if(sim == 's' & flag_line_card == 1 & flag_menu == 0){ //Проверка на текущее наличие подробной информации(если она уже показывается, то 2 раз не надо) + проверка, что мы не в меню
+            flag_line_card = 0;     //Уже не в линии
+            out_detailed_films(root_film_catalog);
+        }
+
+
+
+        //Если пользователь не в меню (т.е. в подробной информации или просто в линии карточек), он может добавить фильм в избранное
+        if(sim == 'e' & flag_menu == 0){
+            return_film_favorites(root_film_catalog);        //Пустая функция, принимает значение возвращаемого фильма
+            printf("                                               Фильм добавлен в избранное");
+            sleep(2);
+            system( "cls" );
+
+            if(flag_line_card == 1){           //В зависимости от того в каком блоке, выводится только линия или подробная информация
+                out_list_films(root_film_catalog);
+            }
+            else{
+                out_list_films(root_film_catalog);
+                out_detailed_films(root_film_catalog);
+            }
+        }
+
+
+
+        //Закрытие подробной информации(пользователь находится на просмотре подробной информации)
+        if(sim == 'w' & flag_line_card == 0 & flag_menu == 0){
+            flag_line_card = 1;
+            system( "cls" );
+            out_list_films(root_film_catalog);
+        }
+
+        //Открытие меню(пользователь находится на линии карточек)
+        else if(sim == 'w' & flag_line_card == 1){
+            flag_line_card = 0;
+            flag_menu = 1;
+            system( "cls" );
+            out_menu();
+        }
+
+        //Условие выхода, желательно реализовать через меню
+        if(sim == 'q'){
+            break;
         }
     }
 
